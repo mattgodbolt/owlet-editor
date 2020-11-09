9a7e1028a3308f12f493725e5aa52ed1b46294fe
Matt Godbolt <matt@godbolt.org>
2020-11-09 08:55:36 -0600

    Ctrl-enter to run, preserve old program

--- src/index.js
+++ src/index.js
@@ -5,34 +5,63 @@ import {Emulator} from './emulator';
 
 import './screech.less';
 
+let screech = null;
+
+const DefaultProgram = [
+    'PRINT "HELLO WORLD"',
+    'GOTO 10'
+].join('\n');
+
+class Screech {
+    constructor() {
+        const editorPane = document.getElementById('editor');
+        this.editor = monacoEditor.create(editorPane, {
+            value: localStorage.getItem("program") || DefaultProgram,
+            minimap: {
+                enabled: false
+            },
+            lineNumbers: l => l * 10,
+            language: 'BBCBASIC',
+            theme: 'vs-dark',
+            fontFamily: "ModeSeven",
+            fontSize: 16,
+            scrollBeyondLastLine: false,
+            wordWrap: 'on',
+            lineDecorationsWidth: 0
+        });
+
+        this.editor.addAction({
+            id: 'execute-basic',
+            label: 'Run',
+            keybindings: [monaco.KeyMod.CtrlCmd | monaco.KeyCode.Enter],
+            keybindingContext: null,
+            contextMenuGroupId: 'navigation',
+            contextMenuOrder: 1.5,
+            run: async () => await this.emulator.runProgram(this.editor.getModel().getValue()),
+        });
+
+        this.editor.getModel().onDidChangeContent(() => {
+            localStorage.setItem("program", this.editor.getModel().getValue());
+        });
+        this.emulator = new Emulator($('#emulator'));
+    }
+
+    async updateProgram() {
+        await this.emulator.runProgram(this.editor.getModel().getValue());
+    }
+
+    async initialise() {
+        await this.emulator.initialise();
+        await this.updateProgram();
+        $(".toolbar .run").click(async () => this.updateProgram());
+    }
+}
+
 async function initialise() {
     registerBbcBasicLanguage();
 
-    const editorPane = document.getElementById('editor');
-    const editor = monacoEditor.create(editorPane, {
-        value: [
-            'PRINT "HELLO WORLD"',
-            'GOTO 10'
-        ].join('\n'),
-        minimap: {
-            enabled: false
-        },
-        lineNumbers: l => l * 10,
-        language: 'BBCBASIC',
-        theme: 'vs-dark',
-        fontFamily: "ModeSeven",
-        fontSize: 16,
-        scrollBeyondLastLine: false,
-        wordWrap: 'on',
-        lineDecorationsWidth: 0
-    });
-
-    const emulator = new Emulator($('#emulator'));
-    await emulator.initialise();
-    await emulator.runProgram(editor.getModel().getValue());
-    $(".toolbar .run").click(async () => {
-        await emulator.runProgram(editor.getModel().getValue());
-    });
+    screech = new Screech();
+    await screech.initialise();
 }
 
 initialise().then(() => {
