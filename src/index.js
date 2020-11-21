c51a74b3b7f61d85f57b98a4e8e4a524f36c5405
Matt Godbolt <matt@godbolt.org>
2020-11-21 10:31:24 -0600

    Separate out owlet from index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,9 @@
 import $ from 'jquery';
-import {editor as monacoEditor, KeyCode, KeyMod} from 'monaco-editor';
 import {registerBbcBasicLanguage} from './bbcbasic';
-import {Emulator} from './emulator';
 import rootHtml from './root.html';
-import Examples from './examples.yaml';
-import Tokens from './tokens';
+import {OwletEditor} from "./owlet";
 
 import './owlet-editor.less';
-
-const DefaultProgram = [
-    'PRINT "HELLO WORLD"',
-    'GOTO 10'
-].join('\n');
-
-const TweetMaximum = 280;
 
 function programUrl(id) {
     if (window.location.hostname === 'localhost')
@@ -25,143 +15,6 @@ async function loadCachedProgram(id) {
     const response = await fetch(programUrl(id));
     const basicText = await response.text();
     return response.status === 200 ? basicText : `REM BBC BASIC program ${id} not found\n`;
-}
-
-class OwletEditor {
-    constructor(optionalInitialProgram) {
-        const program = optionalInitialProgram ? optionalInitialProgram : localStorage.getItem("program") || DefaultProgram;
-        const editorPane = $('#editor');
-        this.editStatus = $('#edit_status');
-        this.emuStatus = $('#emu_status');
-        this.observer = new ResizeObserver(() => this.editor.layout());
-        this.observer.observe(editorPane.parent()[0]);
-        this.editor = monacoEditor.create(editorPane[0], {
-            value: program,
-            minimap: {
-                enabled: false
-            },
-            lineNumbers: l => l * 10,
-            language: 'BBCBASIC',
-            theme: 'vs-dark',
-            renderWhitespace: "none", // seems to fix odd space/font interaction
-            fontSize: 16,
-            scrollBeyondLastLine: false,
-            wordWrap: 'on',
-            lineDecorationsWidth: 0
-        });
-
-        this.editor.addAction({
-            id: 'execute-basic',
-            label: 'Run',
-            keybindings: [KeyMod.CtrlCmd | KeyCode.Enter],
-            keybindingContext: null,
-            contextMenuGroupId: 'navigation',
-            contextMenuOrder: 1.5,
-            run: async () => await this.emulator.runProgram(this.editor.getModel().getValue()),
-        });
-
-        this.editor.getModel().onDidChangeContent(() => {
-            const basicText = this.getBasicText();
-            localStorage.setItem("program", basicText);
-            this.updateStatus(basicText);
-        });
-        this.emulator = new Emulator($('#emulator'));
-        this.updateStatus(program);
-
-        this.examples = {};
-        for (const example of Examples.examples)
-            this.addExample(example);
-    }
-
-    async chooseExample(id) {
-        const example = this.examples[id];
-        if (example.basic) {
-            this.editor.getModel().setValue(example.basic);
-            await this.updateProgram();
-        }
-    }
-
-    addExample(example) {
-        this.examples[example.id] = example;
-        const $examples = $('#examples');
-        const newElem =
-            $examples.find("div.template")
-                .clone()
-                .removeClass("template")
-                .appendTo($examples);
-        newElem.find(".name")
-            .text(example.name)
-            .click(() => this.chooseExample(example.id));
-        newElem.find(".description").text(example.description);
-        if (example.basic)
-            newElem.find(".code").text(example.basic);
-    }
-
-    getBasicText() {
-        return this.editor.getModel().getValue();
-    }
-
-    async updateProgram() {
-        await this.emulator.runProgram(this.getBasicText());
-    }
-
-    detokenize(text) {
-        let output = "";
-        let instr = false;
-        for (let i = 0; i < text.length; i++) {
-            const g = text.codePointAt(i) & 0xff;
-            if (g === 0x22) {
-                // we're a string
-                instr = !instr;
-            }
-            if (g === 0x10 || g === 0x3A) {
-                instr = false
-            }
-            output += (g >= 0x80 && !instr) ? Tokens[g - 0x81] : text[i];
-        }
-        return output;
-    }
-
-    updateStatus(basicText) {
-        this.editStatus
-            .find(".count")
-            .text(basicText.length)
-            .toggleClass("too_long", basicText.length >= TweetMaximum);
-        this.emuStatus.text("BBC Micro Model B | GXR ROM");
-    }
-
-    selectView(selected) {
-        for (const element of ['screen', 'about', 'examples']) {
-            $(`#${element}`).toggle(element === selected);
-        }
-        if (selected === 'screen')
-            this.emulator.start();
-        else
-            this.emulator.pause();
-    }
-
-    share() {
-        const shareModal = document.getElementById("share");
-        shareModal.style.display = "block";
-    }
-
-    async initialise() {
-        await this.emulator.initialise();
-        await this.updateProgram();
-        const actions = {
-            run: async () => {
-                await this.updateProgram();
-                this.selectView('screen')
-            },
-            examples: () => this.selectView('examples'),
-            jsbeeb: () => window.open(`https://bbc.godbolt.org/?embedBasic=${encodeURIComponent(this.getBasicText())}&rom=gxr.rom`, "_blank"),
-            tweet: () => this.share(),
-            emulator: () => this.selectView('screen'),
-            about: () => this.selectView('about'),
-            detokenize: () => this.editor.getModel().setValue(this.detokenize(this.getBasicText()))
-        };
-        $(".toolbar button").click(e => actions[e.target.dataset.action]());
-    }
 }
 
 async function initialise() {
