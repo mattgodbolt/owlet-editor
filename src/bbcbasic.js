dc16afff1ffe78f963aabfde52d52e70111ef9e8
Matt Godbolt <matt@godbolt.org>
2020-11-21 17:26:55 -0600

    Big bag of monarch fixes.
    * Don't use monarch token lists, instead building our own regex.
      Closes #7
    * Sub-language for asm. Not well fleshed out, but a start.
      Closes #1.
    * Adds a bunch more handling for operators.
      Closes #10
    
    Closes #4 too.

--- src/bbcbasic.js
+++ src/bbcbasic.js
@@ -1,6 +1,11 @@
 import {languages} from 'monaco-editor/esm/vs/editor/editor.api';
 import {tokens} from './tokens';
 
+function escape(token) {
+    return token.replace("$", "\\$").replace("(", "\\(");
+}
+
+
 export function registerBbcBasicLanguage() {
     languages.register({id: 'BBCBASIC'});
 
@@ -8,41 +13,43 @@ export function registerBbcBasicLanguage() {
     languages.setMonarchTokensProvider('BBCBASIC', {
         defaultToken: 'invalid',
         brackets: [
-            ['[', ']', 'delimiter.square'],
             ['(', ')', 'delimiter.parenthesis'],
         ],
-        keywords: tokens,
 
         operators: [
             '#', // immediate
             '+', '-', '*', '/', '<<', '>>', '^', '=', '==', '<>', '!=', '<', '>', '<=', '>=',
-            '{', '}', ':'
+            '{', '}', ':', '$', '?', ';', ','
         ],
-        symbols: /[-+#=><!*/{}:]+/,
+        symbols: /[-+#=><!*/{}:?$;,]+/,
         tokenizer: {
             root: [
-                // identifiers and keywords
-                [/[a-zA-Z_$][\w$]*/, {
-                    cases: {
-                        '@keywords': 'keyword',
-                        '@operators': 'operator',
-                        '@default': 'identifier'
-                    }
-                }],
-                [/,\s*[XY]/, 'keyword'],
+                // This is slower than using the "tokens" built in to monarch but
+                // doesn't require whitespace delimited tokens.
+                [tokens
+                    .filter(x => x)
+                    .map(escape)
+                    .sort((x, y) => y.length - x.length)
+                    .join("|"),
+                    'keyword'],
+                [/[a-zA-Z_][\w]*[$%]?/, 'type.identifier'],
                 // whitespace
                 {include: '@whitespace'},
-                // labels
-                [/\.[a-zA-Z_$][\w$]*/, 'type.identifier'],
+                {include: '@common'},
+                ['\\[', {token: 'delimiter.square', next: '@asm'}]
+            ],
+            common: [
                 // immediate
                 [/@symbols/, {cases: {'@operators': 'operator', '@default': ''}}],
                 // numbers
                 [/\d*\.\d+([eE][-+]?\d+)?/, 'number.float'],
                 [/&[0-9a-fA-F]+/, 'number.hex'],
                 [/\d+/, 'number'],
-                [/[{}()[\]]/, '@brackets'],
+                [/[{}()]/, '@brackets'],
                 // strings
-                [/["\u201c\u201d]/, {token: 'string.quote', next: '@string'}]
+                [/["\u201c\u201d]/, {token: 'string.quote', next: '@string'}],
+                // Unusual cases
+                ['@%', 'type.identifier'],
             ],
             whitespace: [
                 [/[ \t\r\n]+/, 'white'],
@@ -51,6 +58,17 @@ export function registerBbcBasicLanguage() {
             string: [
                 [/[^"\u201c\u201d]+/, 'string'],
                 [/["\u201c\u201d]C?/, {token: 'string.quote', next: '@pop'}]
+            ],
+            asm: [
+                // Not exactly working properly yet...but a start
+                [/[a-zA-Z]{3}/, 'keyword'],
+                [/[ \t\r\n]+/, 'white'],
+                [/[;\\].*/, 'comment'],
+                {include: '@common'},
+                [/,\s*[XY]/, 'keyword'],
+                // labels
+                [/\.[a-zA-Z_$][\w$]*/, 'type.identifier'],
+                [']', {token: 'delimiter.square', next: '@pop'}]
             ]
         }
     });
