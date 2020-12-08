2f992c2447b3979e43854d7927fd79cff6190ac3
8bitkick <8bitkick@gmail.com>
2020-12-07 21:06:16 -0800

    () not )(

--- src/owlet.js
+++ src/owlet.js
@@ -181,7 +181,7 @@ export class OwletEditor {
     }
 
     toStateString(basicText) {
-        return encodeURIComponent(JSON.stringify(OwletEditor.stateForBasicProgram(basicText))).replace(/[)]/g, "%28").replace(/[(]/g, "%29");
+        return encodeURIComponent(JSON.stringify(OwletEditor.stateForBasicProgram(basicText))).replace(/[(]/g, "%28").replace(/[)]/g, "%29");
     }
 
     setState(state) {
