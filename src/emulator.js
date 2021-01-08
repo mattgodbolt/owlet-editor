fcdb95938025ec7e0d53d4eb12ee6c395a5fe81a
Matt Godbolt <matt@godbolt.org>
2021-01-08 15:23:47 -0600

    Support keyboard on focus

--- src/emulator.js
+++ src/emulator.js
@@ -17,12 +17,12 @@ const ClocksPerSecond = (2 * 1000 * 1000) | 0;
 const MaxCyclesPerFrame = ClocksPerSecond / 10;
 const urlParams = new URLSearchParams(window.location.search);
 
-let   modelName = "BBC Micro Model B";
+let modelName = "BBC Micro Model B";
 const Model = models.findModel("B");
 
 if (!urlParams.get("experimental")) {
     Model.os.push("gxr.rom");
-    modelName += " | GXR ROM"
+    modelName += " | GXR ROM";
 }
 
 class ScreenResizer {
@@ -33,10 +33,12 @@ class ScreenResizer {
         this.desiredAspectRatio = origWidth / origHeight;
         this.minHeight = origHeight / 4;
         this.minWidth = origWidth / 4;
-        try{
-        this.observer = new ResizeObserver(() => this.resizeScreen());
-        this.observer.observe(this.screen.parent()[0]);} catch(e){console.log(e);};
-
+        try {
+            this.observer = new ResizeObserver(() => this.resizeScreen());
+            this.observer.observe(this.screen.parent()[0]);
+        } catch (e) {
+            console.log(e);
+        }
         this.resizeScreen();
     }
 
@@ -96,10 +98,15 @@ export class Emulator {
             config
         );
 
-        setInterval(this.timer.bind(this),1000)
+        screen.keyup(event => this.keyUp(event));
+        screen.keydown(event => this.keyDown(event));
+        screen.blur(() => this.clearKeys());
+        setInterval(this.timer.bind(this), 1000);
         this.lastFrameTime = 0;
         this.onAnimFrame = _.bind(this.frameFunc, this);
         this.ready = false;
+
+        this.lastShiftLocation = this.lastAltLocation = this.lastCtrlLocation = 0;
     }
 
     async initialise() {
@@ -108,7 +115,7 @@ export class Emulator {
     }
 
     timer() {
-      this.emuStatus.innerHTML = modelName+" | "+this.cpu.cycleSeconds+" s";
+        this.emuStatus.innerHTML = modelName + " | " + this.cpu.cycleSeconds + " s";
     }
 
     start() {
@@ -122,12 +129,12 @@ export class Emulator {
     }
 
     async beebjit(tokenised) {
-        function copyRegion(data, startAddr, endAddr){
-          for (let i = startAddr; i <= endAddr; i++) {
-              processor.writemem(i, data.charCodeAt(i));
-          }
+        function copyRegion(data, startAddr, endAddr) {
+            for (let i = startAddr; i <= endAddr; i++) {
+                processor.writemem(i, data.charCodeAt(i));
+            }
         }
-        
+
         const basic = btoa(tokenised).replace(/\+/g, "-").replace(/\//g, "_").replace(/=+$/, "");
         const processor = this.cpu;
         const response = await fetch(
@@ -138,10 +145,10 @@ export class Emulator {
                 },
             }
         );
-        let beebjitData = await response.json();
-        let data = window.atob(beebjitData.data);
+        const beebjitData = await response.json();
+        const data = window.atob(beebjitData.data);
         copyRegion(data, 0x1900, 0x7fff);
-        this.cpu.cycleSeconds = 60*60*3;
+        this.cpu.cycleSeconds = 60 * 60 * 3;
     }
 
     async runProgram(tokenised) {
@@ -210,5 +217,100 @@ export class Emulator {
             maxx - this.rightMargin + teletextAdjustX,
             maxy - this.bottomMargin
         );
+    }
+
+    keyDown(event) {
+        if (!this.running) return;
+
+        const code = this.keyCode(event);
+        const processor = this.cpu;
+        if (code === utils.keyCodes.HOME && event.ctrlKey) {
+            this.pause();
+        } else if (code === utils.keyCodes.F12 || code === utils.keyCodes.BREAK) {
+            processor.setReset(true);
+        } else {
+            processor.sysvia.keyDown(code, event.shiftKey);
+        }
+        event.preventDefault();
+    }
+
+    clearKeys() {
+        const processor = this.cpu;
+        if (processor && processor.sysvia) processor.sysvia.clearKeys();
+    }
+
+    keyUp(event) {
+        // Always let the key ups come through.
+        const code = this.keyCode(event);
+        const processor = this.cpu;
+        if (processor && processor.sysvia) processor.sysvia.keyUp(code);
+        if (!this.running) return;
+        if (code === utils.keyCodes.F12 || code === utils.keyCodes.BREAK) {
+            processor.setReset(false);
+        }
+        event.preventDefault();
+    }
+
+    keyCode(event) {
+        const ret = event.which || event.charCode || event.keyCode;
+        const keyCodes = utils.keyCodes;
+        switch (event.location) {
+            default:
+                // keyUp events seem to pass location = 0 (Chrome)
+                switch (ret) {
+                    case keyCodes.SHIFT:
+                        return this.lastShiftLocation === 1
+                            ? keyCodes.SHIFT_LEFT
+                            : keyCodes.SHIFT_RIGHT;
+                    case keyCodes.ALT:
+                        return this.lastAltLocation === 1 ? keyCodes.ALT_LEFT : keyCodes.ALT_RIGHT;
+                    case keyCodes.CTRL:
+                        return this.lastCtrlLocation === 1
+                            ? keyCodes.CTRL_LEFT
+                            : keyCodes.CTRL_RIGHT;
+                }
+                break;
+            case 1:
+                switch (ret) {
+                    case keyCodes.SHIFT:
+                        this.lastShiftLocation = 1;
+                        return keyCodes.SHIFT_LEFT;
+
+                    case keyCodes.ALT:
+                        this.lastAltLocation = 1;
+                        return keyCodes.ALT_LEFT;
+
+                    case keyCodes.CTRL:
+                        this.lastCtrlLocation = 1;
+                        return keyCodes.CTRL_LEFT;
+                }
+                break;
+            case 2:
+                switch (ret) {
+                    case keyCodes.SHIFT:
+                        this.lastShiftLocation = 2;
+                        return keyCodes.SHIFT_RIGHT;
+
+                    case keyCodes.ALT:
+                        this.lastAltLocation = 2;
+                        return keyCodes.ALT_RIGHT;
+
+                    case keyCodes.CTRL:
+                        this.lastCtrlLocation = 2;
+                        return keyCodes.CTRL_RIGHT;
+                }
+                break;
+            case 3: // numpad
+                switch (ret) {
+                    case keyCodes.ENTER:
+                        return utils.keyCodes.NUMPADENTER;
+
+                    case keyCodes.DELETE:
+                        return utils.keyCodes.NUMPAD_DECIMAL_POINT;
+                }
+                break;
+        }
+
+        return ret;
     }
 }
